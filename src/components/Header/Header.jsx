1b26bfebe5db71b9c53368151b503c3d8b0499b8
Babik Farmanyan <babikfarmanyan5@gmail.com>
2022-07-01 15:59:20 +0400

    added header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import { NavLink as Link } from "react-router-dom"
+import './Header.css';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="header">
+      <div className="header__logo">
+        <Link to={'/'}><span>My</span>Cinema</Link>
+      </div>
+{/* 
+      <button className="burger-btn"><i className="fa-solid fa-bars"></i></button>
+
+      <div className="burger-menu">
+        <div className="burger-menu__icons">
+            <i className="fa-solid fa-bars"></i>
+            <Link to={'/'}><span>My</span>Cinema</Link>
+        </div>
+        <div className="burger-menu__body ">
+            <Link to={'/'}>Home</Link>
+            <Link to={'/categories/movies'}>Movies</Link>
+            <Link to={'/categories/serials'}>Serials</Link>
+            <Link to={'/categories/cartoons'}>Cartoons</Link>
+        </div>
+
+      </div> */}
+
+      <div className="header__navbar">
+          <Link to={'/'}>Home</Link>
+          <Link to={'/categories/movies'}>Movies</Link>
+          <Link to={'/categories/serials'}>Serials</Link>
+          <Link to={'/categories/cartoons'}>Cartoons</Link>
+      </div>
+
+      <div className="header__icon">
+        <form>
+        <input type="search" className="header__search" placeholder="Search..."/>
+        </form>
+
+        <i className="fa-regular fa-heart"></i>
+        <i className="fa-regular fa-user"></i>
+      </div>
+
+    </div>
   )
 }
 
