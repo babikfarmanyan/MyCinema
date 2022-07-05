de10ce301fb8c3f7eae11240797f88aa2f28f4ff
Meline <meline.9196@gmail.com>
2022-07-05 22:16:49 +0400

    Detail

--- src/pages/Detail/Detail.jsx
+++ src/pages/Detail/Detail.jsx
@@ -6,31 +6,27 @@ import "./Detail.css";
 
 const Detail = () => {
   const [watchDetail, setWatchData] = useState([]);
+  
   const { id, catName } = useParams('');
   const navigate = useNavigate();
-  let backdrop_path;
 
   useEffect(() => {
     getDetailById(id, catName).then(data => {
-      setWatchData(data);
-
-      backdrop_path = 'backdrop_path' in data ? getOriginalImg(data.backdrop_path) : 'https://atalian.co.th/wp-content/uploads/sites/21/2019/07/4_Keeping-Up-with-the-Cinema-Cleanliness.png'
-      console.log('backdrop_path' in data );
-      console.log('bp=',backdrop_path);
+      setWatchData(data); 
     })
 
 
-  }, [id])
+  }, [])
 
   return (
     watchDetail.id &&
     <div className='detail'>
-      <img className='backdrop_path' src={backdrop_path} alt="" />
+      <img className='backdrop_path' src={watchDetail.backdrop_path ? getOriginalImg(watchDetail.backdrop_path) : 'https://atalian.co.th/wp-content/uploads/sites/21/2019/07/4_Keeping-Up-with-the-Cinema-Cleanliness.png'} alt="" />
       <div className="content">
         <img className='poster_path' src={getOriginalImg(watchDetail.poster_path)} alt="" />
         <div className="content-text">
           <h2>{catName === 'movies' ? watchDetail.title : watchDetail.name}</h2>
-          <h2>{watchDetail.id}</h2>
+          
           <p>{watchDetail.overview}</p>
           <h2>{watchDetail.popularity}</h2>
         </div>
