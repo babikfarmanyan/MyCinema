d8ec9a91561f5ff4e1dc725caf5d0c4e603b996b
Babik Farmanyan <babikfarmanyan5@gmail.com>
2022-07-06 22:21:25 +0400

    changed Home page

--- src/components/CategoryList/CaregoryList.jsx
+++ src/components/CategoryList/CaregoryList.jsx
@@ -1,32 +1,32 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import { NavLink as Link } from "react-router-dom"
-import { getMostPopular, getGanres } from '../../config';
+import { getMostPopular, getGanres, getTopRated } from '../../config';
 
 import CategoryItem from '../CategoryItem/CategoryItem';
 
 import './CategoryList.css'
 
-const CaregoryList = ({catName}) => {
+const CaregoryList = ({catName, topRated}) => {
 
   const [watchData, setWatchData] = useState([]);
   const [genres, setGenres] = useState([]);
 
   useEffect(() => {
-    getMostPopular(catName).then(data => setWatchData(data.results.slice(0, 5)));
+    topRated ? getTopRated(catName).then(data => setWatchData(data.results.slice(0, 6))): getMostPopular(catName).then(data => setWatchData(data.results.slice(0, 5)));
     getGanres(catName).then(genres => setGenres(genres.genres));
   }, [])
 
   return (
     <section className="category">
-      <Link className='category__title' to={`/categories/${catName}`}>{catName}</Link>
+      {!topRated ? <Link className='category__title link' to={`/categories/${catName}`}>{catName} by genre</Link>: <h2 className='category__title'>Top rated {catName}</h2>}
       <div className='category__list'>
         {
           watchData.map(watchItem => (
             <CategoryItem key={watchItem.id} watchItem={watchItem} genres={genres} />
           ))
         }
-      <Link className='seemore' to={`/categories/${catName}`}>SEE MORE</Link>
+      {!topRated && <Link className='seemore' to={`/categories/${catName}`}>SEE MORE</Link>}
       </div>
     </section>
   )
