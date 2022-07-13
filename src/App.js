16a0a26f9c74334204005db210995f17a9944c17
PetrosyanZ <petrosyanzaven310@gmail.com>
2022-07-13 10:38:51 +0400

    not found

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 // Components
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import NotFound from './components/NotFound/NotFound';
 // Pages
 import Home from './pages/Home/Home';
 import Categories from './pages/Categories/Categories';
@@ -17,8 +18,9 @@ function App() {
           <Routes>
               <Route path='/' element={<Home />}/>
               <Route path='/categories/:name' element={<Categories />}/>
-              <Route path='/:catName/:id' element={<Detail />}></Route>
+              <Route path='/:catName/:id' element={<Detail />} />
               <Route path='/about' element={<About />} />
+              <Route path='*' element={<NotFound />} />
           </Routes>
         <Footer/>
       </Router>
