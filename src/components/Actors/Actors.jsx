025e8658f54b6681bc0bf7eeddd48c3cfd825adb
Meline <meline.9196@gmail.com>
2022-07-17 11:51:42 +0400

    mediaActor

--- src/components/Actors/Actors.jsx
+++ src/components/Actors/Actors.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {getOriginalImg} from '../../config';
+import "./Actors.css"
 
 const Actors = ({actorsList}) => {
 
@@ -8,19 +9,24 @@ const Actors = ({actorsList}) => {
     <div className="actorsList">
           {actorsList.map(item => {
                   return (  (item.profile_path)  ? <React.Fragment key={item.id}> 
-                                                       <div className='actors'>
+                                                       <div className='actor'>
                                                           <img className='imgActor' src={getOriginalImg(item.profile_path)} alt=""/> 
-                                                          <h2>{item.name}</h2>
-                                                          <h3>{item.character}</h3>
+                                                          <div className="name">
+                                                                      <h2>{item.name}</h2>
+                                                                      <h3>{item.character}</h3>
+                                                                    </div>
                                                         </div> </React.Fragment>
                                                   :              
                                                     <React.Fragment key={item.id}> 
-                                                              <div className='actors'>
+                                                              <div className='actor'>
                                                                   <div className="notImg">
                                                                       <span>{item.name[0]}</span>
                                                                     </div> 
-                                                                    <h2>{item.name}</h2>
-                                                                    <h3>{item.character}</h3>
+                                                                    <div className="name">
+                                                                      <h2>{item.name}</h2>
+                                                                      <h3>{item.character}</h3>
+                                                                    </div>
+                                                                    
                                                                 </div>
                                                     </React.Fragment>
                            )
